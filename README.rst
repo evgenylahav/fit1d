a8bdb54744b6c55fd2461032c0d6a675c33abf80
evgenylahav <31128049+evgenylahav@users.noreply.github.com>
2019-02-24 10:02:56 +0200

    first commit

---
+++ README.rst
@@ -0,0 +1,8 @@
+1D Fit Package
+===============
+
+This 1D fit package will be used as an infra package for the system software apps
+
+✨🍰✨
+
+To install it just clone it from git and run $make init
---
+++ fit1d/generic_model.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+import numpy as np
+from typing import Any, Dict
+
+
+class GenericModel(ABC):
+    @staticmethod
+    @abstractmethod
+    def fit(x: np.ndarray, y: np.ndarray, **kwargs) -> Dict[str, Any]:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def eval(model: Dict[str, Any], x: np.ndarray) -> np.ndarray:
+        pass
+
+
+class GenericModelMock(GenericModel):
+    @staticmethod
+    def fit(x: np.ndarray, y: np.ndarray, **kwargs) -> Dict[str, Any]:
+        return {'a': 5, 'b': "hello"}
+
+    @staticmethod
+    def eval(model: Dict[str, Any], x: np.ndarray) -> np.ndarray:
+        return np.array([1, 2, 3, 4])
