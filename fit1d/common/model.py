41366692ae230205128364dc8fb988dc26146f3c
Evgeny Lahav <Evgeny_Lahav@amat.com>
2019-03-11 16:43:05 +0200

    added typing to allow model input of union of float and list of floats

--- fit1d/common/model.py
+++ fit1d/common/model.py
@@ -3,7 +3,7 @@ The model class is an common class used to define models for different types of 
 Each fit will need to implement its own model class
 """
 from abc import ABC, abstractmethod
-from typing import Dict, List
+from typing import Dict, List, Union
 
 
 class Model(ABC):
@@ -11,23 +11,23 @@ class Model(ABC):
     The Model class is used to contain a dictionary of model parameters and provides
     a few services to handle model objects easily.
     """
-    _model_parameters: Dict[str, float] = dict()
+    _model_parameters: Dict[str, Union[List[float], float]] = dict()
 
     def __eq__(self, other):
         return self._model_parameters == other._model_parameters
 
     @abstractmethod
-    def from_list(self, model_list: List[float]):
+    def create_model_from_list(self, model_list: List[float]):
         pass
 
     @abstractmethod
-    def to_list(self) -> List[float]:
+    def dump_model_to_list(self) -> List[float]:
         pass
 
-    def from_dict(self, model_dict: Dict[str, float]):
+    def create_model_from_dict(self, model_dict: Dict[str, Union[List[float], float]]):
         self._model_parameters = model_dict
 
-    def to_dict(self) -> Dict[str, float]:
+    def dump_model_to_dict(self) -> Dict[str, Union[List[float], float]]:
         return self._model_parameters
 
 
@@ -36,11 +36,11 @@ class ModelMock(Model):
     def __init__(self, model_parameters):
         self._model_parameters = model_parameters
 
-    def from_list(self, model_list: List[float]):
+    def create_model_from_list(self, model_list: List[float]):
         self._model_parameters["param1"] = model_list[0]
         self._model_parameters["param2"] = model_list[1]
 
-    def to_list(self):
+    def dump_model_to_list(self):
         out_list = list()
         out_list.append(self._model_parameters["param1"])
         out_list.append(self._model_parameters["param2"])
